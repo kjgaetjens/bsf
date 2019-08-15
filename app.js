9c7458151cc826a987e9063699c8e72a19024fc8
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-08-15 18:10:00 -0500

    added neo4j driver

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const app = express()
+const neo4j = require('neo4j-driver').v1;
+const user = "neo4j"
+const password = "123"
+const uri = "bolt://localhost:7687"
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+const session = driver.session();
+
+const resultPromise = session.run(
+  'MATCH (c:Character) WHERE c.name = "Blanka" RETURN c'
+)
+
+resultPromise.then(result => {
+  session.close();
+
+  const singleRecord = result.records[0];
+  const node = singleRecord.get(0);
+
+  console.log(node.properties.name);
+
+  // on application exit:
+  driver.close();
+});
 
 
 app.use(express.static('static'))
