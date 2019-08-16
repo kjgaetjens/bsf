3f139e6386e992838d1929764006050c687477e9
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-08-15 23:12:37 -0500

    css selector name change

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const app = express()
-const path = require('path')
 const neo4j = require('neo4j-driver').v1;
 const user = "neo4j"
-const password = "bsfdc19"
+const password = "123"
 const uri = "bolt://localhost:7687"
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 const session = driver.session();
 const path = require('path')
-
 
 
 const VIEWS_PATH = path.join(__dirname, 'views')
@@ -18,7 +16,6 @@ const VIEWS_PATH = path.join(__dirname, 'views')
 
 
 
-<<<<<<< HEAD
 app.use(express.urlencoded())
 app.use(express.static('static'));
 app.engine('mustache',mustacheExpress())
@@ -50,21 +47,6 @@ app.get('/buttons/:buttontype/characters',(req,res)=>{
         
             
     // })
-=======
-const VIEWS_PATH = path.join(__dirname, 'views')
-
-// tell express to use mustache templating engine
-app.engine('mustache',mustacheExpress())
-app.set('views',VIEWS_PATH)
-app.set('view engine','mustache')
-
-
-app.get('/buttons', (req,res)=>{
-    res.render('buttons_selection')
-})
-
-app.get('/buttons/:buttontype/characters', (req, res) => {
->>>>>>> master
     res.render('character_selection')
     
 })
@@ -80,12 +62,14 @@ app.get('/buttons/:buttontype/characters/:charactername/results', (req,res)=>{
     let buttonTypeParam = req.params.buttontype
    let controlType = ''
    let direction = ''
+
    if((buttonTypeParam).includes('joystick')) {
        controlType = buttonTypeParam.substring(0,buttonTypeParam.indexOf('-'))
        direction = buttonTypeParam.substring(buttonTypeParam.indexOf('-')+1,buttonTypeParam.length)
    } else {
        controlType = buttonTypeParam
    }
+
    let characterName = req.params.charactername
     
     let resultPromise = session.run(
@@ -105,30 +89,12 @@ app.get('/buttons/:buttontype/characters/:charactername/results', (req,res)=>{
         // on application exit:
         driver.close();
 
-        res.render('results')
+        res.render('results', {character: characterName})
 
-<<<<<<< HEAD
 }).catch(error=>console.log(error))
 })
 
-=======
 
-app.get('/buttons/:buttontype/characters/:charactername/results', (req,res)=>{
-    let buttonTypeParam = req.params.buttontype
-    let controlType = ''
-    let direction = ''
-    if((buttonTypeParam).includes('joystick')) {
-        controlType = buttonTypeParam.substring(0,buttonTypeParam.indexOf('-'))
-        direction = buttonTypeParam.substring(buttonTypeParam.indexOf('-')+1,buttonTypeParam.length)
-    } else {
-        controlType = buttonTypeParam
-    }
-    let characterName = req.params.charactername
-
-    //write code to return info from neo4j
->>>>>>> master
-
-})
 
 app.listen(3000, ()=>{
     console.log('server has started')
