e63c6ffe09b8744cdfe16862be0bc8971365a791
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-08-15 21:48:39 -0500

    modified routers

--- app.js
+++ app.js
@@ -7,58 +7,82 @@ const password = "123"
 const uri = "bolt://localhost:7687"
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 const session = driver.session();
-
-const resultPromise = session.run(
-  'MATCH (c:Character) RETURN c'
-)
+const path = require('path')
 
 
 
+const VIEWS_PATH = path.join(__dirname, 'views')
 
-app.use(express.static('static'))
+
+
+
 
 app.use(express.urlencoded())
-
+app.use(express.static('static'));
 app.engine('mustache',mustacheExpress())
-app.set('views', './views')
+app.set('views', VIEWS_PATH)
 app.set('view engine','mustache')
 
 app.get('/buttons/:buttontype', (req,res)=>{
     
-    res.render('character_selection')
+    res.render('buttons_selection')
 })
 
-app.get('buttons/:buttontype/characters/:charactername', (req,res)=>{
-//     let characterName = req.params.charName
+app.get('/buttons/:buttontype/characters',(req,res)=>{
+    //     let resultPromise = session.run(
+            
+    //         "MATCH (c:Character) WHERE c.name = $character RETURN c",
+    //         {character: characterName}
+    //       )
+          
+    //     resultPromise.then(result => {
+    //         session.close();
+          
+    //         const singleRecord = result.records[0];
+    //         const node = singleRecord.get(0);
+          
+    //         let resultsObj = node.properties.name;
+    //         console.log(resultsObj)
+    //         // on application exit:
+    //         driver.close();   
+        
+            
+    // })
+    res.render('character_selection')
     
-//     let resultPromise = session.run(
-        
-//         "MATCH (c:Character) WHERE c.name = $character RETURN c",
-//         {character: characterName}
-//       )
-      
-//     resultPromise.then(result => {
-//         session.close();
-      
-//         const singleRecord = result.records[0];
-//         const node = singleRecord.get(0);
-      
-//         let resultsObj = node.properties.name;
-        
-//         // on application exit:
-//         driver.close();
-
-//         res.redirect('results', resultsObj)
-
-// }).catch(error=>console.log(error))
-
-// })
-    res.render('character_selection')
 })
+
+
+// app.get('buttons/:buttontype/characters/:charactername', (req,res)=>{
+//     let characterName = req.params.charactername
+//     res.send('asdfg')
+// })
 
 
 app.get('/buttons/:buttontype/characters/:charactername/results', (req,res)=>{
-    res.render('results')
+    let characterName = req.params.charactername
+    let buttonType = req.params.buttontype
+    
+    let resultPromise = session.run(
+        "MATCH (c:Character) WHERE c.name = $character RETURN c",
+        {character: characterName}
+      )
+      
+    resultPromise.then(result => {
+        session.close();
+      
+        const singleRecord = result.records[0];
+        const node = singleRecord.get(0);
+      
+        let resultsObj = node.properties.name;
+        console.log(resultsObj)
+        
+        // on application exit:
+        driver.close();
+
+        res.render('results')
+
+}).catch(error=>console.log(error))
 })
 
 
