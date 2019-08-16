c28acebe8abae18d762f5b5891d00fc0c75d5bbd
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-08-15 19:49:06 -0500

    updated buttons_selection.mustache with forms

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mustacheExpress = require('mustache-express')
 const app = express()
 const neo4j = require('neo4j-driver').v1;
 const user = "neo4j"
-const password = "123"
+const password = "bsfdc19"
 const uri = "bolt://localhost:7687"
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 const session = driver.session();
@@ -33,6 +33,37 @@ app.engine('mustache',mustacheExpress())
 app.set('views', './views')
 app.set('view engine','mustache')
 
+// const getControlsSelection = () => {
+//   let brokenButtons = []
+
+//   const down = document.getElementById('down-joystick')
+//   const right = document.getElementById('right-joystick')
+//   const upleft = document.getElementById('upleft-joystick')
+//   const upright = document.getElementById('upright-joystick')
+//   const up = document.getElementById('up-joystick')
+//   const left = document.getElementById('left-joystick')
+//   const downleft = document.getElementById('downleft-joystick')
+//   const one = document.getElementById('one-btn')
+//   const two = document.getElementById('two-btn')
+//   const three = document.getElementById('three-btn')
+//   const four = document.getElementById('four-btn')
+//   const five = document.getElementById('five-btn')
+//   const six = document.getElementById('six-btn')
+
+
+
+//   down.addEventListener('click', () => {
+//     if (!brokenButtons.includes('down')) {
+//       brokenButtons.push('down')
+//     }
+//   })
+
+//   console.log(brokenButtons)
+//   //what to return if joystick itself is broken? just make them select arrows instead?
+// }
+
+
+
 app.get('/buttons', (req,res)=>{
     res.render('buttons_selection')
 })
@@ -44,8 +75,6 @@ app.get('/characters', (req,res)=>{
 app.get('/results', (req,res)=>{
     res.render('results')
 })
-
-
 
 app.listen(3000, ()=>{
     console.log('server has started')
